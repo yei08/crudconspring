2d355ded418a496e0e779525ea83c9ff7d3f8e73
yeifer alcala <yeiferalcala23@gmail.com>
2024-11-23 01:06:13 -0500

    commit hasta la lista(guia 7)

--- src/main/java/com/desarrolloWeb/crud/ControladorInicio.java
+++ src/main/java/com/desarrolloWeb/crud/ControladorInicio.java
@@ -3,12 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.desarrolloWeb.crud;
+import com.desarrolloWeb.crud.modelo.Usuario;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.desarrolloWeb.crud.modelo.Usuario;
 /**
  *
  * @author JEIFER ALCALA
@@ -25,7 +27,7 @@ public class ControladorInicio {
         modelo.addAttribute("dato", dato);
         Usuario u = new Usuario();
         u.setId("123");
-        u.setPassword(dato);
+        u.setPassword("4234234");
         u.setNombre("yeifer");
         u.setApellidos("Alcalá Mestre");
         u.setRol("estudiante");
@@ -33,8 +35,32 @@ public class ControladorInicio {
         u.setTelefono("4234252523");
         u.setEstado("activo");
         u.setFecha_registro("2020/03/30");
-        modelo.addAttribute("alguien",u);
+        modelo.addAttribute("alguien", u);
+       
+        Usuario u1 = new Usuario();
+        u1.setId("1234");
+        u1.setPassword("4234234");
+        u1.setNombre("jonh jairo");
+        u1.setApellidos("peñaralta rodriguez");
+        u1.setRol("estudiante");
+        u1.setEmail("john.com");
+        u1.setTelefono("4234252523");
+        u1.setEstado("activo");
+        u1.setFecha_registro("2020/03/30");
+      
         
+        Usuario u2 = new Usuario();
+        u2.setId("1236");
+        u2.setPassword("4234234");
+        u2.setNombre("johanis");
+        u2.setApellidos("Mestre");
+        u2.setRol("estudiante");
+        u2.setEmail("johanis.com");
+        u2.setTelefono("4234252523");
+        u2.setEstado("activo");
+        u2.setFecha_registro("2020/03/30");
+        List<Usuario> listaUsuarios = Arrays.asList(u1, u2);
+        modelo.addAttribute("usuarios",listaUsuarios);
         log.info("ejecutando el controlador inicio MVC");
         return "index";
     }
